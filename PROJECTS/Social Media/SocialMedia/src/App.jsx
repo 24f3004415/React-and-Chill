57d6f25262a1232339ba6b1f44fe3e8bccd34a23
Mohit Rai <mohiit.rai.007@gmail.com>
2025-10-01 22:17:12 +0530

    Footer Component Pasted

--- PROJECTS/Social Media/SocialMedia/src/App.jsx
+++ PROJECTS/Social Media/SocialMedia/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import SideBar from './components/SideBar'
 
 const App = () => {
   return (
     <div>
       <Header></Header>
       <Footer></Footer>
+      <SideBar></SideBar>
     </div>
   )
 }
--- PROJECTS/Social Media/SocialMedia/src/components/Footer/Footer.jsx
+++ PROJECTS/Social Media/SocialMedia/src/components/Footer/Footer.jsx
@@ -7,13 +7,13 @@ const Footer = () => {
         <div className={styles.container}>
             <footer className="py-3 my-4 text-white-50">
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 ">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 ">Features</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 ">Pricing</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 ">FAQs</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 ">About</a></li>
                 </ul>
-                <p className="text-center text-body-secondary">© 2025 Company, Inc</p>
+                <p className="text-center text-body-light">© 2025 Company, Inc</p>
             </footer>
         </div>
     )
